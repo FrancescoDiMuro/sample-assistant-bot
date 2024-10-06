1b5f7d473c2e73b38bc9171e8473361df592bc04
Francesco Dimuro <francesco.dimuro@adecco.it>
2024-10-07 00:36:15 +0200

    feat: add user model

---
+++ app/models/user/user.py
@@ -0,0 +1,26 @@
+from datetime import datetime, UTC
+from models.base.base import Base
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from uuid import UUID, uuid4
+
+
+class User(Base):
+
+    __tablename__ = "users"
+    
+    # About "default" parameter:
+    # https://docs.sqlalchemy.org/en/20/faq/
+    # ormconfiguration.html#part-two-using-dataclasses-support-with-mappedasdataclass
+    id: Mapped[UUID] = mapped_column(primary_key=True, nullable=False, default=uuid4)
+    first_name: Mapped[str] = mapped_column(nullable=False)
+    last_name: Mapped[str] = mapped_column(nullable=True)
+    username: Mapped[str] = mapped_column(unique=True, nullable=True)
+    telegram_id: Mapped[int] = mapped_column(unique=True, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(nullable=False, default=datetime.now(UTC))
+    updated_at: Mapped[datetime] = mapped_column(nullable=True, onupdate=datetime.now(UTC))
+
+    # These are the relationships between other models
+    # Thanks to these variables, we can access the specified models
+    # through this (User) model
+    location = relationship("Location", back_populates="user")
+    reminders = relationship("Reminder", back_populates="user")
