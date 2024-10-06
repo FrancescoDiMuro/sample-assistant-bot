a077b360ec754f878ab8a1db2e8fc57d80b224e2
Francesco Dimuro <francesco.dimuro@adecco.it>
2024-10-06 09:31:42 +0200

    feat: add default values to bot settings

--- app/__main__.py
+++ app/__main__.py
@@ -2,18 +2,22 @@ from dotenv import load_dotenv
 from handlers.command.start import start
 from os import getenv
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler
-
+from telegram.constants import ParseMode
+from telegram.ext import ApplicationBuilder, CommandHandler, Defaults
 
 # DEBUG
 print(f".env loaded: {load_dotenv()}")
 
 # If the bot token has been found and it is valorized
 if BOT_TOKEN := getenv("BOT_TOKEN"):
+
+    # Add default values to the bot settings
+    defaults = Defaults(parse_mode=ParseMode.HTML)
     
     # Create the app
     app = ApplicationBuilder() \
         .token(BOT_TOKEN) \
+        .defaults(defaults=defaults) \
         .build()
     
     start_handler = CommandHandler(command="start", callback=start)
