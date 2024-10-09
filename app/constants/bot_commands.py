7a78ff668964df4707e6803c57e8d0ef77f1e08c
Francesco Dimuro <francesco.dimuro@adecco.it>
2024-10-09 18:55:51 +0200

    feat: add bot commands

---
+++ app/constants/bot_commands.py
@@ -0,0 +1,23 @@
+# Bot commands visible in the commands menu
+BOT_COMMANDS = [
+    {
+        "command": "/help",
+        "description": "Shows how to use the bot"
+    },
+    {
+        "command": "/news",
+        "description": "Show the news based on the user location"
+    },
+    {
+        "command": "/setlocation",
+        "description": "Set the user location"
+    },
+    {
+        "command": "/todos",
+        "description": "Show the user's to-dos"
+    },
+    {
+        "command": "/weather",
+        "description": "Show the weather information based on the user location"
+    },
+]
---
+++ app/handlers/post_init/post_init.py
@@ -0,0 +1,19 @@
+from constants.bot_commands import BOT_COMMANDS
+from telegram import BotCommand, MenuButton
+from telegram.ext import Application
+
+
+async def post_init(application: Application) -> None:
+
+    # Initialize an empty list of bot commands
+    bot_commands: list = []
+
+    # Set bot commands (on the left side of the input text field)
+    for command in BOT_COMMANDS:
+        
+        # Append the command to the list of bot commands
+        bot_commands.append(BotCommand(**command))
+
+    await application.bot.set_my_commands(bot_commands)
+
+    return None
