5654c1b98ccf3905974a1e2f6da5388712553742
FrancescoDiMuro <dimurofrancesco@virgilio.it>
2024-10-18 09:04:33 +0200

    feat: add todo model

---
+++ app/models/todo/todo.py
@@ -0,0 +1,26 @@
+from datetime import datetime, UTC
+from models.base.base import Base
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from uuid import UUID, uuid4
+
+
+class Todo(Base):
+
+    __tablename__ = "todos"
+    
+    # About "default" parameter:
+    # https://docs.sqlalchemy.org/en/20/faq/
+    # ormconfiguration.html#part-two-using-dataclasses-support-with-mappedasdataclass
+    id: Mapped[UUID] = mapped_column(primary_key=True, nullable=False, default=uuid4)
+    user_id: Mapped[UUID] = mapped_column(ForeignKey("users.id"), nullable=False)
+    details: Mapped[str] = mapped_column(nullable=False)
+    expires_on: Mapped[datetime] = mapped_column(nullable=False)
+    done: Mapped[bool] = mapped_column(nullable=False, default=False)
+    created_at: Mapped[datetime] = mapped_column(nullable=False, default=datetime.now(UTC))
+    updated_at: Mapped[datetime] = mapped_column(nullable=True, onupdate=datetime.now(UTC))
+
+    # These are the relationships between other models
+    # Thanks to these variables, we can access the specified models
+    # through this (Todo) model
+    user = relationship("User", back_populates="todos")
