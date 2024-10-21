99dd9429a9ccb53e2efab8ace7863657770f464c
FrancescoDiMuro <dimurofrancesco@virgilio.it>
2024-10-21 18:50:57 +0200

    fix(retrieve_todo): todo_id is UUID and not int

--- app/models/todo/crud/retrieve.py
+++ app/models/todo/crud/retrieve.py
@@ -1,3 +1,4 @@
+from uuid import UUID
 from database.db import SessionLocal
 from models.todo.todo import Todo
 from sqlalchemy import Select, select
@@ -13,7 +14,7 @@ def retrieve_todos() -> Todo | None:
         return session.scalars(sql_statement).all()
 
 
-def retrieve_todo(todo_id: int) -> Todo | None:
+def retrieve_todo(todo_id: UUID) -> Todo | None:
 
     with SessionLocal() as session:
 
