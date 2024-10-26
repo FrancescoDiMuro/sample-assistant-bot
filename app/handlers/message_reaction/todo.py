b8fb05aa428b302e49d233b722e312990a945429
FrancescoDiMuro <dimurofrancesco@virgilio.it>
2024-10-27 01:03:42 +0200

    feat: add "message reaction handler" to handle todo completion with a message reaction

---
+++ app/handlers/message_reaction/todo.py
@@ -0,0 +1,70 @@
+from datetime import UTC, datetime
+from constants.emoji import Emoji
+from models.todo.crud.retrieve import retrieve_todo
+from models.todo.crud.update import update_todo
+from telegram import Update
+from telegram.ext import (
+    ApplicationHandlerStop, 
+    ContextTypes, 
+    MessageReactionHandler
+)
+
+
+async def mark_todo_as_done(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+    # Get the various information for further processing
+    message_reaction_updated = update.message_reaction
+    message_id = message_reaction_updated.message_id
+    user_telegram_id = message_reaction_updated.user.id
+
+    # Guards
+    if not (user_data := context.bot_data.get(user_telegram_id)): raise ApplicationHandlerStop()
+    if not (pending_todos := user_data.get("pending_todos")): raise ApplicationHandlerStop()
+    if message_id not in pending_todos.keys(): raise ApplicationHandlerStop()
+
+    # If there's a todo_id assigned to the specified message
+    if todo_id := pending_todos.get(message_id):
+        
+        # If there is a todo with the specified id
+        if todo := retrieve_todo(todo_id=todo_id):
+
+            # If there is a new_reaction for the current message_reaction_updated
+            if reaction := message_reaction_updated.new_reaction:
+
+                # Get the 0-th reaction's emoji
+                emoji = reaction[0].emoji
+            
+                # If the to-do is not in the state of "done",
+                # and the user's reaction to the todo-reminder is a "thumbs-up"
+                if todo.done == False and emoji == Emoji.THUMBS_UP_SIGN:
+
+                    # Prepare the data to update
+                    todo_data: dict = {
+                        "done": True
+                    }
+
+                    # Update the todo
+                    if update_todo(
+                        todo_id=todo_id,
+                        todo_data=todo_data
+                    ):
+                        
+                        # Delete the pending todo
+                        context.bot_data[user_telegram_id]["pending_todos"].pop(message_id)
+
+                        # User text
+                        user_text = (
+                            f"To-Do checked as completed on "
+                            f"{datetime.now(tz=UTC):%Y-%m-%d %H:%M} (UTC)"
+                        )
+                        
+                        await context.bot.send_message(
+                            chat_id=user_telegram_id,
+                            text=user_text,
+                            reply_to_message_id=message_id
+                        )
+
+
+# Create the handler
+message_reaction_handler = MessageReactionHandler(callback=mark_todo_as_done)
+    
