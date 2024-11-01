97389342a9ec36730fbf0697edcc4f5fb8acab5f
FrancescoDiMuro <dimurofrancesco@virgilio.it>
2024-11-01 12:39:36 +0100

    refactor: code refactoring
    Add user_id parameter to the retrieve_todos function
    
    Correct callback_data for each todo
    
    Add else in case there are no todos
    
    Add table headers

--- app/handlers/command/todos.py
+++ app/handlers/command/todos.py
@@ -1,14 +1,16 @@
 from __future__ import annotations
+from datetime import datetime, timedelta
 from constants.emoji import Emoji
 from models.todo.crud.retrieve import retrieve_todos
 from models.user.crud.retrieve import retrieve_user
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton, ReplyKeyboardMarkup, Update
-from telegram.ext import ContextTypes, CommandHandler
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.constants import ChatAction
+from telegram.ext import ContextTypes, CommandHandler
 
 
 async def todos(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
+    # Send chat action to inform the user that there's a process ongoing
     context.bot.send_chat_action(
         chat_id=update.effective_chat.id,
         action=ChatAction.TYPING
@@ -18,19 +20,40 @@ async def todos(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     if user := retrieve_user(user_telegram_id=update.effective_user.id):
 
         # Get the uncompleted list of user's todos
-        if user_uncompleted_todos := retrieve_todos(is_done=False):
+        if user_uncompleted_todos := retrieve_todos(user_id=user.id, is_done=False):
 
-            # Create the list of todos (details) for the inline keyboard
             keyboard: list = [
                 [
                     InlineKeyboardButton(
-                        text=todo.details, 
-                        callback_data=todo.id.hex
-                    )
+                        text=f"{Emoji.CALENDAR} Due to", 
+                        callback_data="placeholder"
+                    ),
+                    InlineKeyboardButton(
+                        text=f"{Emoji.OPEN_BOOK} Details", 
+                        callback_data="placeholder"
+                    ),
                 ]
-                for todo in user_uncompleted_todos
             ]
 
+            # Create the list of todos (details) for the inline keyboard
+            for todo in user_uncompleted_todos:
+
+                todo_user_due_date: datetime = \
+                    f"{todo.due_date + timedelta(seconds=todo.utc_offset):%Y-%m-%d %H:%M}"
+                
+                keyboard.append(
+                    [
+                        InlineKeyboardButton(
+                            text=todo_user_due_date, 
+                            callback_data="placeholder"
+                        ),
+                        InlineKeyboardButton(
+                            text=todo.details, 
+                            callback_data=f"todo_details:{todo.id.hex}"
+                        )
+                    ]
+                )
+                    
             # Create the keyboard
             todos_inline_keyboard = InlineKeyboardMarkup(
                 inline_keyboard=keyboard,
@@ -40,11 +63,14 @@ async def todos(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
                 f"{Emoji.OPEN_BOOK} To-dos list:"
             )
 
-            await update.message.reply_text(
+        else:
+
+            user_text = f"{Emoji.PERSON_SHRUGGING} There are no to-dos!"
+
+        await update.message.reply_text(
                 text=user_text,
                 reply_markup=todos_inline_keyboard
             )
-
 
 todos_handler = CommandHandler(
     command="todos",
