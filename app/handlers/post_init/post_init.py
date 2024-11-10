5e6e115872b031915e5f7a3390e99b9216393755
FrancescoDiMuro <dimurofrancesco@virgilio.it>
2024-11-10 23:25:26 +0100

    refactor(post_init): update logic to set bot commands

--- app/handlers/post_init/post_init.py
+++ app/handlers/post_init/post_init.py
@@ -10,10 +10,10 @@ async def post_init(application: Application) -> None:
     bot_commands: list = []
 
     # Set bot commands (on the left side of the input text field)
-    for command in BOT_COMMANDS:
+    for command, description in BOT_COMMANDS.items():
         
         # Append the command to the list of bot commands
-        bot_commands.append(BotCommand(**command))
+        bot_commands.append(BotCommand(command=command, description=description))
 
     # Set bot commands
     await application.bot.set_my_commands(bot_commands)
